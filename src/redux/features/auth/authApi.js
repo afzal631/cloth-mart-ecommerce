5ecf3705748312a107c23e54ff08707b9a4b0262
mohammed afzal <afzalmd03@gmail.com>
2024-09-07 14:45:50 +0530

    changing base url of backend.

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api",
+    baseUrl: "https://cloth-mart-ecommerce-backend.onrender.com/api",
     credentials: "include",
   }),
   tagTypes: ["User"],
